d7be50622227ba1087c77e6494ce48a168fb87c9
Oyaki122 <oc.yaki122@gmail.com>
2023-10-30 14:49:44 +0900

    udp版clientを実装

--- client/client.c
+++ client/client.c
@@ -6,11 +6,13 @@
 /*                                                                  */
 
 #include "../icslab2_net.h"
+#include <sys/socket.h>
+#include <time.h>
 
-int main(int argc, char** argv) {
-  char* server_ipaddr_str = "127.0.0.1"; /* サーバIPアドレス（文字列） */
-  unsigned int port = TCP_SERVER_PORT; /* ポート番号 */
-  char* filename = "test.txt";
+int main(int argc, char **argv) {
+  char *server_ipaddr_str = "127.0.0.1"; /* サーバIPアドレス（文字列） */
+  unsigned int port = UDP_SERVER_PORT; /* ポート番号 */
+  char *filename = "test.txt";
 
   int sock;                      /* ソケットディスクリプタ */
   struct sockaddr_in serverAddr; /* サーバ＝相手用のアドレス構造体 */
@@ -29,9 +31,10 @@ int main(int argc, char** argv) {
     server_ipaddr_str = argv[1];
   if (argc > 2) /* 宛先を指定のIPアドレス、portにする */
     port = (unsigned int)atoi(argv[2]);
-  if (argc > 3) filename = argv[3];
+  if (argc > 3)
+    filename = argv[3];
 
-  FILE* file = fopen(filename, "w");
+  FILE *file = fopen(filename, "w");
 
   /* STEP 1: 宛先サーバのIPアドレスとポートを指定する */
   memset(&serverAddr, 0, sizeof(serverAddr)); /* 0クリア */
@@ -45,22 +48,24 @@ int main(int argc, char** argv) {
   printf("ip address: %s\n", inet_ntoa(addr));
   printf("port#: %d\n", ntohs(serverAddr.sin_port));
 
-  /* STEP 2x: TCPソケットをオープンする */
-  sock = socket(AF_INET, SOCK_STREAM, 0);
+  /* STEP 2x: UDPソケットをオープンする */
+  sock = socket(AF_INET, SOCK_DGRAM, 0);
   if (sock < 0) {
     perror("socket");
     return 1;
   }
 
-  /* ここで、ローカルでsocketをbind()してもよいが省略する */
+  struct timespec start_time, end_time;
+  clock_gettime(CLOCK_REALTIME, &start_time);
 
-  /* STEP 3x: サーバに接続する（bind相当も実行） */
-  if (connect(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-    perror("connect");
-    return 1;
+  char nullData = 0;
+  if (sendto(sock, &nullData, sizeof(char), 0, (struct sockaddr *)&serverAddr,
+             sizeof(struct sockaddr)) != 1) {
+    perror("request send");
+    return -1;
   }
 
-  int total = 0;
+  uint32_t total = 0, recvCounter;
   /* STEP 4xxx: サーバからデータグラムを受けとる */
   while ((n = read(sock, buf, BUF_LEN - 1)) > 0) {
     buf[n] = '\0';
@@ -68,6 +73,29 @@ int main(int argc, char** argv) {
     fwrite(buf, sizeof(char), n, file);
     total += n;
   }
+
+  struct sockaddr clientAddr;
+  socklen_t addrLen = sizeof(struct sockaddr); /* serverAddrのサイズ */
+  int endCounter = 0;
+  while ((n = recvfrom(sock, buf, BUF_LEN, 0, (struct sockaddr *)&clientAddr,
+                       &addrLen)) > 0) {
+    if (strncmp(buf, "end\n", BUF_LEN)) {
+      endCounter++;
+      if(endCounter >= 10) break;
+    }
+    fwrite(buf, sizeof(char), n, file);
+    total += n;
+  }
+  clock_gettime(CLOCK_REALTIME, &end_time);
+
+  double elapsed_time_ms = (end_time.tv_sec - start_time.tv_sec) * 1e3 +
+                           (end_time.tv_nsec - start_time.tv_nsec) / 1e6;
+
+  printf("total: %d bytes\n", total);
+  printf("elapsed time: %6.5lf ms\n", elapsed_time_ms);
+  printf("throughput: %6.1lf kbps\n",
+         total * 8 / elapsed_time_ms * 1000 / 1000);
+  printf("received %d packets\n", recvCounter);
 
   /* STEP 6: ソケットのクローズ */
   close(sock);
@@ -79,4 +107,3 @@ int main(int argc, char** argv) {
 
 /* Local Variables: */
 /* compile-command: "gcc tcp_echo_client.c -o tcp_echo_client.out " */
-
