45bbbc19f16cf6d4025b6b5825caea4c73cf55e4
Oyaki122 <oc.yaki122@gmail.com>
2023-11-06 11:11:43 +0900

    ちょっと変更

--- server/server.c
+++ server/server.c
@@ -7,15 +7,14 @@
 
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-#define CLIENT_IP "172.29.0.50" // Fixed client IP address to send data
+#define CLIENT_IP "172.29.0.50"  // Fixed client IP address to send data
 #define ACK "END\n"
 #define ACK_LOOP 500
 
 #define DST_SIZE 5
-#define ACK_COUNT 15
+#define ACK_COUNT 50
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   int sock;                      /* ソケットディスクリプタ */
   struct sockaddr_in serverAddr; /* サーバ＝自分用アドレス構造体 */
   struct sockaddr_in clientAddr; /* クライアント＝相手用アドレス構造体 */
@@ -34,12 +33,11 @@ int main(int argc, char **argv)
 
   int i; /* ループ用変数 */
 
-  int destNode[DST_SIZE] = {5};        // 送信先ノード
-  double ratio[DST_SIZE] = {1};        // 送信先ノードへの割合
-  int sentDestCounter[DST_SIZE] = {0}; // 送信先ノードへの送信パケット数
+  int destNode[DST_SIZE] = {5};  // 送信先ノード
+  double ratio[DST_SIZE] = {1};  // 送信先ノードへの割合
+  int sentDestCounter[DST_SIZE] = {0};  // 送信先ノードへの送信パケット数
 
-  for (i = 0; i <= DST_SIZE; i++)
-  {
+  for (i = 0; i <= DST_SIZE; i++) {
     struct addrinfo hints, *info;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
@@ -50,8 +48,7 @@ int main(int argc, char **argv)
   }
 
   /* コマンドライン引数の処理 */
-  if (argc % 2 != 0)
-  {
+  if (argc % 2 != 0) {
     printf(
         "Usage: %s [input_txt] [node_id1] [ratio1] [node_id2] [ratio2] ...\n",
         argv[0]);
@@ -60,8 +57,7 @@ int main(int argc, char **argv)
 
   int destInputNum = MIN((argc - 2) / 2, DST_SIZE);
   double ratioSum = 0;
-  for (int i = 0; i < destInputNum; i++)
-  {
+  for (int i = 0; i < destInputNum; i++) {
     destNode[i] = atoi(argv[2 * i + 2]);
     ratio[i] = atof(argv[2 * i + 3]) + ratioSum;
     ratioSum = ratio[i];
@@ -73,8 +69,7 @@ int main(int argc, char **argv)
   fd = open(input_txt, O_RDONLY);
 
   /* STEP 1: UDPソケットをオープンする */
-  if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-  {
+  if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
     perror("socket");
     return 1;
   }
@@ -88,8 +83,7 @@ int main(int argc, char **argv)
   serverAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* どのIPアドレス宛でも */
 
   /* STEP 3:ソケットとアドレスをbindする */
-  if (bind(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
-  {
+  if (bind(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
     perror("bind");
     return 1;
   }
@@ -101,8 +95,7 @@ int main(int argc, char **argv)
   /* クライアントから情報を取得 */
   n = recvfrom(sock, buf, BUF_LEN, 0, (struct sockaddr *)&clientAddr,
                (socklen_t *)&addrLen);
-  if (n < 0)
-  {
+  if (n < 0) {
     perror("recvfrom");
     return (1);
   }
@@ -114,22 +107,18 @@ int main(int argc, char **argv)
   printf("port#: %d\n", ntohs(clientAddr.sin_port));
 
   int sentPacket = 0, sentByte = 0;
-  while ((n = read(fd, buf, BUF_LEN)) > 0)
-  {
+  while ((n = read(fd, buf, BUF_LEN)) > 0) {
     double randomDest = (double)rand() / (RAND_MAX + 1.0) * ratioSum;
     int dest;
-    for (int i = 0; i < destInputNum; i++)
-    {
-      if (randomDest < ratio[i])
-      {
+    for (int i = 0; i < destInputNum; i++) {
+      if (randomDest < ratio[i]) {
         dest = destNode[i];
         sentDestCounter[i]++;
         break;
       }
     }
     if (sendto(sock, buf, n, 0, (sendtoAddr[dest - 1])->ai_addr,
-               sizeof(struct sockaddr)) != n)
-    {
+               sizeof(struct sockaddr)) != n) {
       perror("sendto");
       return (1);
     }
@@ -144,8 +133,8 @@ int main(int argc, char **argv)
   // /* 終了したというシグナルを送り元に対して返す */
   // for (i = 0; i < ACK_LOOP; i++)
   // {
-  //   // if (sendto(sock, ACK, 4, 0, (struct sockaddr *)&clientAddr, addrLen) != 4)
-  //   if (sendto(sock, ACK, n, 0, (sendtoAddr[dest - 1])->ai_addr,
+  //   // if (sendto(sock, ACK, 4, 0, (struct sockaddr *)&clientAddr, addrLen)
+  //   != 4) if (sendto(sock, ACK, n, 0, (sendtoAddr[dest - 1])->ai_addr,
   //              sizeof(struct sockaddr)) != 4)
   //   {
   //     perror("sendto");
@@ -154,31 +143,26 @@ int main(int argc, char **argv)
   //   usleep(1000);
   // }
   // データ送信後の処理でACK送信のループ
-  for (int i = 0; i < ACK_COUNT; i++)
-  {
+  for (int i = 0; i < ACK_COUNT; i++) {
     double randomDest = (double)rand() / (RAND_MAX + 1.0) * ratioSum;
     int destIndex = -1;
-    for (int j = 0; j < destInputNum; j++)
-    {
-      if (randomDest < ratio[j])
-      {
+    for (int j = 0; j < destInputNum; j++) {
+      if (randomDest < ratio[j]) {
         destIndex = j;
         break;
       }
     }
-    if (destIndex >= 0 && destIndex < destInputNum)
-    {
-      struct sockaddr_in *dest_sockaddr = (struct sockaddr_in *)(sendtoAddr[destNode[destIndex] - 1]->ai_addr);
+    if (destIndex >= 0 && destIndex < destInputNum) {
+      struct sockaddr_in *dest_sockaddr =
+          (struct sockaddr_in *)(sendtoAddr[destNode[destIndex] - 1]->ai_addr);
       char *dest_ip = inet_ntoa(dest_sockaddr->sin_addr);
       printf("Send ACK to Node %d at IP: %s\n", destNode[destIndex], dest_ip);
 
-      if (sendto(sock, ACK, strlen(ACK), 0, (struct sockaddr *)dest_sockaddr, sizeof(struct sockaddr)) != strlen(ACK))
-      {
+      if (sendto(sock, ACK, strlen(ACK), 0, (struct sockaddr *)dest_sockaddr,
+                 sizeof(struct sockaddr)) != strlen(ACK)) {
         perror("sendto");
       }
-    }
-    else
-    {
+    } else {
       printf("Error: No valid destination for ACK\n");
     }
     usleep(10);
@@ -187,8 +171,7 @@ int main(int argc, char **argv)
   printf("EOT transmitted to client\n");
   //}
   //
-  for (int i = 0; i < destInputNum; i++)
-  {
+  for (int i = 0; i < destInputNum; i++) {
     printf("destNode: %d, sentPacket: %d\n", destNode[i], sentDestCounter[i]);
   }
 
